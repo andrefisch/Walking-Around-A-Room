034d432e8b38b8270291790d718f8d65bde8877c
Andrew Fischl <anfischl@gmail.com>
2017-08-28 15:33:03 -0400

    first commit

---
+++ MovingAroundARoom/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+    
+    public GameObject player;
+    private Vector3 offset;
+
+	void Start () 
+    {
+		offset = transform.position - player.transform.position;
+	}
+	
+    // runs every frame but is guaranteed to run after all items have been processed in update. this ensures player has already moved for that frame
+	void LateUpdate () 
+    {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ MovingAroundARoom/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour 
+{
+    // not using forces so update is better than fixed update
+	void Update () 
+    {
+        // any object that rotates should have a RigidBody component otherwise performance will suffer
+		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	}
+}
